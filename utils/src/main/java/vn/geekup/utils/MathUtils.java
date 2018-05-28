00cdfe32de4e2099bc3eea490056b86df7af651f
Quoc Trung <quoctrung66@gmail.com>
2018-05-28 21:41:12 +0700

    init project

---
+++ utils/src/main/java/vn/geekup/utils/MathUtils.java
@@ -0,0 +1,18 @@
+package vn.geekup.utils;
+
+/**
+ * Created by quoctrung on 5/28/18.
+ */
+
+public class MathUtils {
+
+    /**
+     * Sum of two number
+     * @param a int
+     * @param b int
+     * @return Sum of two number
+     */
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
